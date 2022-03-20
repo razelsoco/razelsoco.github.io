d47791ac85e485fb1417b625465ca46ddf7d682d
Razel <razelsoco@gmail.com>
2022-03-19 23:06:19 -0700

    use formspree

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -103,7 +103,7 @@
 
           // POST values in the background the the script URL
           $.ajax({
-              type: "GET",
+              type: "POST",
               url: url,
               data: $(this).serialize(),
               success: function (data)
